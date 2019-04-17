9361f8f91c5f26d206ae2197a9d500fb7e4d24f5
rfm <r@robertmasen.pizza>
2019-04-17 07:39:52 -0500

    runner for part 1 working

---
+++ crates/example-runner/src/main.rs
@@ -0,0 +1,16 @@
+use wasmer_runtime::{
+    imports,
+    instantiate,
+};
+// For no we are going to use this to read in our wasm bytes
+static WASM: &[u8] = include_bytes!("../../../target/wasm32-unknown-unknown/debug/example_plugin.wasm");
+
+fn main() {
+    // Instantiate the web assembly module
+    let instance = instantiate(WASM, &imports!{}).expect("failed to instantiate wasm module");
+    // Bind the add function from the module
+    let add = instance.func::<(i32, i32), i32>("add").expect("failed to bind function add");
+    // execute the add function
+    let three = add.call(1, 2).expect("failed to execute add");
+    println!("three: {}", three); // "three: 3"
+}

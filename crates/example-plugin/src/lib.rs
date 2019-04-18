2d471f993709912d4c39fab2bf91e442a564fde9
rfm <r@robertmasen.pizza>
2019-04-17 22:11:14 -0500

    update second plugin

--- crates/example-plugin/src/lib.rs
+++ crates/example-plugin/src/lib.rs
@@ -1,6 +1,21 @@
 // ./crates/example-plugin/src/lib.rs
 
+/// This is the actual code we would 
+/// write if this was a pure rust
+/// interaction
+pub fn length(s: &str) -> u32 {
+    s.len() as u32
+}
+
+/// Since it isn't we need a way to
+/// translate the data from wasm
+/// to rust
 #[no_mangle]
-pub fn add(one: i32, two: i32) -> i32 {
-    one + two
+pub fn _length(ptr: i32, len: u32) -> u32 {
+    // Extract the string from memory.
+    let value = unsafe { 
+        String::from_raw_parts(ptr as *mut u8, len as usize, len as usize)
+    };
+    //pass the value to `length` and return the result
+    length(&value)
 }

cc1fa5cd2052c8d975b0f725f8d3b7616c7d4d3c
Robert Masen <rmasen@apexrevtech.com>
2019-04-19 15:58:58 -0500

    part-3.2

--- crates/example-macro/src/lib.rs
+++ crates/example-macro/src/lib.rs
@@ -1,8 +1,39 @@
 // ./crates/example-macro/src/lib.rs
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use syn::{
+   Item as SynItem,
+};
+use proc_macro2::{
+   Ident,
+   Span,
+};
+use quote::quote;
+
+#[proc_macro_attribute]
+pub fn plugin_helper(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    let tokens2 = proc_macro2::TokenStream::from(tokens);
+    let parse2 = syn::parse2::<SynItem>(tokens2).expect("Failed to parse tokens");
+    match parse2 {
+        SynItem::Fn(func) => handle_func(func),
+        _ => panic!("Only functions are currently supported")
     }
+}
+
+fn handle_func(func: syn::ItemFn) -> TokenStream {
+    // Copy this function's identifier
+    let ident = func.ident.clone();
+    // Create a new identifier with a underscore in front of 
+    // the original identifier
+    let shadows_ident = Ident::new(&format!("_{}", ident), Span::call_site());
+    // Generate some rust with the original and new
+    // shadowed function
+    let ret = quote! {
+        #func
+
+        pub fn #shadows_ident() {
+            #ident();
+        }
+    };
+    ret.into()
 }
--- crates/example-plugin/src/lib.rs
+++ crates/example-plugin/src/lib.rs
@@ -1,39 +1,7 @@
 // ./crates/example-plugin/src/lib.rs
-use bincode::{deserialize, serialize};
-/// This is the actual code we would 
-/// write if this was a pure rust
-/// interaction
-pub fn multiply(pair: (u8, String)) -> (u8, String) {
-    // create a repeated version of the string
-    // based on the u8 provided
-    let s = pair.1.repeat(pair.0 as usize);
-    // Multiply the u8 by the length
-    // of the new string
-    let u = pair.0.wrapping_mul(s.len() as u8);
-    (u, s)
-}
+use example_macro::*;
 
-/// Since it isn't we need a way to
-/// translate the data from wasm
-/// to rust
-#[no_mangle]
-pub fn _multiply(ptr: i32, len: u32) -> i32 {
-    // Extract the string from memory.
-    let slice = unsafe { 
-        ::std::slice::from_raw_parts(ptr as _, len as _)
-    };
-    // deserialize the memory slice
-    let pair = deserialize(slice).expect("Failed to deserialize tuple");
-    // Get the updated version
-    let updated = multiply(pair);
-    // serialize the updated value
-    let ret = serialize(&updated).expect("Failed to serialize tuple");
-    // Capture the length
-    let len = ret.len() as u32;
-    // write the length to byte 1 in memory
-    unsafe {
-        ::std::ptr::write(1 as _, len);
-    }
-    // return the start index
-    ret.as_ptr() as _
+#[plugin_helper]
+pub fn attributed() {
+    println!("attributed!")
 }

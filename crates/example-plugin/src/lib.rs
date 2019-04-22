45fb012bb8ac7601177ef1382dc3dfc899d6b2ac
Robert Masen <rmasen@apexrevtech.com>
2019-04-22 14:18:24 -0500

    match examples in part 3

--- crates/example-plugin/src/lib.rs
+++ crates/example-plugin/src/lib.rs
@@ -1,7 +1,16 @@
 // ./crates/example-plugin/src/lib.rs
+use bincode::{deserialize, serialize};
 use example_macro::*;
-
+/// This is the actual code we would 
+/// write if this was a pure rust
+/// interaction
 #[plugin_helper]
-pub fn attributed() {
-    println!("attributed!")
+pub fn multiply(pair: (u8, String)) -> (u8, String) {
+    // create a repeated version of the string
+    // based on the u8 provided
+    let s = pair.1.repeat(pair.0 as usize);
+    // Multiply the u8 by the length
+    // of the new string
+    let u = pair.0.wrapping_mul(s.len() as u8);
+    (u, s)
 }

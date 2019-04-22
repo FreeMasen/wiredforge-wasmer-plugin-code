de8b5e02e5f927ea7209ec5c2a1b1ececb0533d3
Robert Masen <rmasen@apexrevtech.com>
2019-04-22 14:25:12 -0500

    match part 3 examples

--- crates/example-macro/src/lib.rs
+++ crates/example-macro/src/lib.rs
@@ -12,8 +12,12 @@ use quote::quote;
 
 #[proc_macro_attribute]
 pub fn plugin_helper(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    // convert the TokenStream into proc_macro2::TokenStream
     let tokens2 = proc_macro2::TokenStream::from(tokens);
+    // parse the TokenStream into a syn::Item
     let parse2 = syn::parse2::<SynItem>(tokens2).expect("Failed to parse tokens");
+    // Check if it is a function
+    // if not panic
     match parse2 {
         SynItem::Fn(func) => handle_func(func),
         _ => panic!("Only functions are currently supported")
@@ -21,7 +25,7 @@ pub fn plugin_helper(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
 }
 
 fn handle_func(func: syn::ItemFn) -> TokenStream {
-    // Copy this function's identifier
+    // Copy the function's identifier
     let ident = func.ident.clone();
     // Create a new identifier with a underscore in front of 
     // the original identifier
@@ -32,7 +36,7 @@ fn handle_func(func: syn::ItemFn) -> TokenStream {
         #func
 
         pub fn #shadows_ident() {
-            #ident((2, "attributed"));
+            #ident((2, String::from("attributed")));
         }
     };
     ret.into()

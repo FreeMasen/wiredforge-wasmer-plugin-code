d9cdf015775f135076dba0b367eddfb2adb61805
rfm <r@robertmasen.pizza>
2019-04-28 19:09:25 -0500

    part-4.2

--- crates/example-plugin/src/lib.rs
+++ crates/example-plugin/src/lib.rs
@@ -1,15 +1,30 @@
 // ./crates/example-plugin/src/lib.rs
 use wasmer_plugin_example::*;
-/// This is the actual code we would 
-/// write if this was a pure rust
-/// interaction
+use mdbook::{
+    book::{
+        Book,
+        BookItem,
+    },
+    preprocess::PreprocessorContext,
+};
 #[plugin_helper]
-pub fn multiply(pair: (u8, String)) -> (u8, String) {
-    // create a repeated version of the string
-    // based on the u8 provided
-    let s = pair.1.repeat(pair.0 as usize);
-    // Multiply the u8 by the length
-    // of the new string
-    let u = pair.0.wrapping_mul(s.len() as u8);
-    (u, s)
+pub fn preprocess((_ctx, mut book): (PreprocessorContext, Book)) -> Book {
+    // Iterate over the book's sections assigning
+    // the updated items to the book we were passed
+    book.sections = book.sections.into_iter().map(|s| {
+        // each section could be a chapter
+        // or a seperator
+        match s {
+            // if its a chapter, we want to update that
+            BookItem::Chapter(mut ch) => {
+                // replace all WASMs with Wasms
+                ch.content = ch.content.replace("WASM", "Wasm");
+                // Wrap the contents back up into a Chapter
+                BookItem::Chapter(ch)
+            },
+            _ => s,
+        }
+    }).collect();
+    // Return the updated book
+    book
 }
--- crates/example-runner/src/main.rs
+++ crates/example-runner/src/main.rs
@@ -13,12 +13,20 @@ use std::{
     }
 };
 use mdbook::{
-    book::{
-        Book,
-        BookItem,
-    },
+    book::Book,
     preprocess::PreprocessorContext,
 };
+use bincode::{
+    serialize,
+    deserialize,
+};
+use wasmer_runtime::{
+    instantiate,
+    imports,
+};
+
+// For now we are going to use this to read in our wasm bytes
+static WASM: &[u8] = include_bytes!("../../../target/wasm32-unknown-unknown/debug/example_plugin.wasm");
 
 static USAGE: &str = "
 Usage:
@@ -60,23 +68,73 @@ fn main() {
 
 /// Update the book's contents so that all WASMs are
 /// replaced with Wasm
-fn preprocess(_ctx: PreprocessorContext, mut book: Book) -> Result<Book, String> {
-    // Iterate over the book's sections assigning
-    // the updated items to the book we were passed
-    book.sections = book.sections.into_iter().map(|s| {
-        // each section could be a chapter
-        // or a seperator
-        match s {
-            // if its a chapter, we want to update that
-            BookItem::Chapter(mut ch) => {
-                // replace all WASMs with Wasms
-                ch.content = ch.content.replace("WASM", "Wasm");
-                // Wrap the contents back up into a Chapter
-                BookItem::Chapter(ch)
-            },
-            _ => s,
-        }
-    }).collect();
-    // Return the updated book
-    Ok(book)
+fn preprocess(ctx: PreprocessorContext, book: Book) -> Result<Book, String> {
+    println!("creating instance");
+    let instance = instantiate(&WASM, &imports!{}).expect("failed to instantiate wasm module");
+    // The changes start here
+    // First we get the module's context
+    println!("capturing context");
+    let context = instance.context();
+    // Then we get memory 0 from that context
+    // web assembly only supports one memory right
+    // now so this will always be 0.
+    println!("capturing memory");
+    let memory = context.memory(0);
+    // Now we can get a view of that memory
+    println!("getting first view");
+    let view = memory.view::<u8>();
+    // Zero our the first 4 bytes of memory
+    println!("zeroing cell 1-4");
+    for cell in view[1..5].iter() {
+        cell.set(0);
+    }
+    let pair = (ctx, book);
+    println!("serializing pair");
+    let bytes = serialize(&pair)
+        .expect("Failed to serialize tuple");
+    // Our length of bytes
+    let len = bytes.len();
+    // loop over the wasm memory view's bytes
+    // and also the string bytes
+    println!("injecting bytes");
+    for (cell, byte) in view[5..len + 5].iter().zip(bytes.iter()) {
+        // set each wasm memory byte to 
+        // be the value of the string byte
+        cell.set(*byte)
+    }
+    println!("binding preprocessor");
+    // Bind our helper function
+    let wasm_preprocess = instance.func::<(i32, u32), i32>("_preprocess")
+        .expect("Failed to bind _multiply");
+    println!("running preprocessor");
+    // Call the helper function an store the start of the returned string
+    let start = wasm_preprocess.call(5 as i32, len as u32)
+        .expect("Failed to execute _multiply") as usize;
+    // Get an updated view of memory
+    let new_view = memory.view::<u8>();
+    // Setup the 4 bytes that will be converted
+    // into our new length
+    let mut new_len_bytes = [0u8;4];
+    for i in 0..4 {
+        // attempt to get i+1 from the memory view (1,2,3,4)
+        // If we can, return the value it contains, otherwise
+        // default back to 0
+        new_len_bytes[i] = new_view
+            .get(i + 1)
+            .map(|c| c.get())
+            .unwrap_or(0);
+    }
+    // Convert the 4 bytes into a u32 and cast to usize
+    let new_len = u32::from_ne_bytes(new_len_bytes) as usize;
+    // Calculate the end as the start + new length
+    let end = start + new_len;
+    // Capture the string as bytes 
+    // from the new view of the wasm memory
+    let updated_bytes: Vec<u8> = new_view[start..end]
+                                    .iter()
+                                    .map(|c|c.get())
+                                    .collect();
+    // Convert the bytes to a string
+    deserialize(&updated_bytes)
+        .map_err(|e| format!("Error deserializing after wasm update\n{}", e))
 }

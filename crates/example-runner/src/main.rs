890745edf903421d125fb9d37af8f19b1e10c546
rfm <r@robertmasen.pizza>
2019-05-02 07:31:55 -0500

    fix context stuff

--- crates/example-runner/src/main.rs
+++ crates/example-runner/src/main.rs
@@ -46,11 +46,11 @@ fn main() {
     }
     // Parse and deserialize the context and book
     // from stdin
-    let (ctx, book): (PreprocessorContext, Book) = 
+    let (_ctx, book): (PreprocessorContext, Book) = 
         from_reader(stdin())
         .expect("Failed to deserialize context and book");
     // Update the book's contents
-    let updated = preprocess(ctx, book)
+    let updated = preprocess(book)
         .expect("Failed to preprocess book");
     // serialize and write the updated book
     // to stdout
@@ -60,7 +60,7 @@ fn main() {
 
 /// Update the book's contents so that all WASMs are
 /// replaced with Wasm
-fn preprocess(_ctx: PreprocessorContext, mut book: Book) -> Result<Book, String> {
+fn preprocess(mut book: Book) -> Result<Book, String> {
     // Iterate over the book's sections assigning
     // the updated items to the book we were passed
     book.sections = book.sections.into_iter().map(|s| {
